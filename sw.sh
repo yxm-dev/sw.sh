2a0bfb3cd6c4d7af9f15fe7d0d4fcec1950a6013
yxm <yxm.dev@gmx.ie>
2023-10-29 10:52:01 -0300

    ...

--- sw.sh
+++ sw.sh
@@ -3,40 +3,40 @@
 install_dir=$HOME/.config/sw.sh
 
 # declaring arrays
-    declare -a sw_topdir
-    declare -a sw_mddir
-    declare -a sw_alias
-    declare -a sw_gitdir
-    declare -a sw_commit
-    declare -a sw_branch
-    declare -a sw_remote
-    declare -a sw_mdDIR
-    declare -a sw_tpl
-    declare -a sw_html
+    declare -a SW_topdir
+    declare -a SW_mddir
+    declare -a SW_alias
+    declare -a SW_gitdir
+    declare -a SW_commit
+    declare -a SW_branch
+    declare -a SW_remote
+    declare -a SW_mdDIR
+    declare -a SW_tpl
+    declare -a SW_html
 # including data
     source $install_dir/data
 # defining variables
-    for i in ${!sw_alias[@]}; do
-        sw_mdDIR[$i]=${sw_topdir[$i]}/${sw_mddir[$i]}
-        sw_tpl[$i]=${sw_topdir[$i]}/tpl/tpl.html
-        sw_html[$i]=${sw_topdir[$i]}/html
+    for i in ${!SW_alias[@]}; do
+        SW_mdDIR[$i]=${SW_topdir[$i]}/${SW_mddir[$i]}
+        SW_tpl[$i]=${SW_topdir[$i]}/tpl/tpl.html
+        SW_html[$i]=${SW_topdir[$i]}/html
     done
     
 # SW FUNCTION
     function sw(){
 ## Auxiliary Functions
-### inverse of "sw_alias" array
-        function inv_sw_alias(){
-            for i in ${!sw_alias[@]}; do
-                if [[ "$1" == "${sw_alias[$i]}" ]]; then
+### inverse of "SW_alias" array
+        function inv_SW_alias(){
+            for i in ${!SW_alias[@]}; do
+                if [[ "$1" == "${SW_alias[$i]}" ]]; then
                     x=$i
                 fi
           done
         }
     
 ### verify if there is "$" symbols and, in this case, call "pandoc-static-katex" filter
-        declare -f sw_math
-        function sw_math(){
+        declare -f SW_math
+        function SW_math(){
             has_math_1=$(grep -o "\$.\+\$" "$1")
             has_math_2=$(grep -o "\$\$.\+\$\$" "$1")
             if [[ ! -z "$has_math_1" ]] || [[ ! -z "$has_math_2" ]]; then
@@ -48,8 +48,8 @@ install_dir=$HOME/.config/sw.sh
             fi
         }
 
-        declare -f sw_option
-        function sw_option(){
+        declare -f SW_option
+        function SW_option(){
             if [[ "$1" == "-n" ]] || [[ "$1" == "-N" ]] ||
                  [[ "$1" == "-ns" ]] || [[ "$1" == "--number" ]] ||
                  [[ "$1" == "--number-sections" ]]; then
@@ -79,14 +79,14 @@ install_dir=$HOME/.config/sw.sh
                  fi
         }
 
-        declare -f sw_cvt        
-        function sw_cvt(){
-            sw_option $3 $4
+        declare -f SW_cvt        
+        function SW_cvt(){
+            SW_option $3 $4
             name=$(basename $1)
             for i in {3..4}; do
-               echo "$i" >> /dev/null
+               echo "$i" > /dev/null
             done
-            sed -r 's/(\[.+\])\(([^)]+)\)/\1(\2.html)/g; s/(\[\[.+\]\])/\1(\1.html)/g' < "$1" | pandoc -s $number_section $toc $option $1 $filter_katex -t html5 $css_katex --template ${sw_tpl[$2]} | sed -r 's/<li>(.*)\[ \]/<li class="todo done0">\1/g; s/<li>(.*)\[X\]/<li class="todo done4">\1/g; s/https:(.*).html/https:\1/g; s/.md.html/.html/g;' > "$name.html"
+            sed -r 's/(\[.+\])\(([^)]+)\)/\1(\2.html)/g; s/(\[\[.+\]\])/\1(\1.html)/g' < "$1" | pandoc -s $number_section $toc $option $1 $filter_katex -t html5 $css_katex --template ${SW_tpl[$2]} | sed -r 's/<li>(.*)\[ \]/<li class="todo done0">\1/g; s/<li>(.*)\[X\]/<li class="todo done4">\1/g; s/https:(.*).html/https:\1/g; s/.md.html/.html/g;' > "$name.html"
         }
 
 ### default error message
@@ -97,44 +97,44 @@ install_dir=$HOME/.config/sw.sh
             if   [[ "$1" == "-c" ]] || [[ "$1" == "-cvt" ]] ||
                  [[ "$1" == "--convert" ]]; then
                 if [[ -z "$2" ]]; then
-                    for i in ${!sw_alias[@]}; do
-                        sw -c ${sw_alias[$i]}
+                    for i in ${!SW_alias[@]}; do
+                        sw -c ${SW_alias[$i]}
                     done
-                elif [[ "${sw_alias[@]}" =~ "$2" ]]; then
+                elif [[ "${SW_alias[@]}" =~ "$2" ]]; then
                     echo "deleting old html files..."
-                    inv_sw_alias $2
-                    rm -r ${sw_html[$x]}
-                    mkdir ${sw_html[$x]}
+                    inv_SW_alias $2
+                    rm -r ${SW_html[$x]}
+                    mkdir ${SW_html[$x]}
                     echo "recreating the html/ dir..."
-                    cp -r ${sw_mdDIR[$x]}/* ${sw_html[$x]}/
-                    sw_htmlfiles=$(find ${sw_html[$x]} -type f -name "*.md")
-                    for f in ${sw_htmlfiles[@]}; do
+                    cp -r ${SW_mdDIR[$x]}/* ${SW_html[$x]}/
+                    SW_htmlfiles=$(find ${SW_html[$x]} -type f -name "*.md")
+                    for f in ${SW_htmlfiles[@]}; do
                         echo "converting $f..."
                         dir=$(dirname $f)
                         cd $dir
-                         sw_math $f
-                         sw_cvt $f $x $3 $4
+                         SW_math $f
+                         SW_cvt $f $x $3 $4
                     done
                     echo "fixing possible errors..."
-                    find ${sw_html[$x]} -type f -name "*.md" -delete
-                    find ${sw_html[$x]} -name '*.md.html' -execdir bash -c 'mv -i "$1" "${1//.md.html/.html}"' bash {} \;
+                    find ${SW_html[$x]} -type f -name "*.md" -delete
+                    find ${SW_html[$x]} -name '*.md.html' -execdir bash -c 'mv -i "$1" "${1//.md.html/.html}"' bash {} \;
                     echo "Done!"
                 else
                     echo $error
                 fi
 ### "--push" option 
             elif [[ "$1" == "-p" ]] || [[ "$1" == "--push" ]]; then
-                if [[ "${sw_alias[@]}" =~ "$2" ]]; then
-                    inv_sw_alias $2
-                    rsync -av --progress --delete  --exclude '.git/*' --exclude '.domains' ${sw_html[$x]}/ ${sw_gitdir[$x]} 
-                    cd ${sw_gitdir[$x]}
+                if [[ "${SW_alias[@]}" =~ "$2" ]]; then
+                    inv_SW_alias $2
+                    rsync -av --progress --delete  --exclude '.git/*' --exclude '.domains' ${SW_html[$x]}/ ${SW_gitdir[$x]} 
+                    cd ${SW_gitdir[$x]}
                     git add .
-                    git commit -m "${sw_commit[$x]}"
-                    git pull ${sw_remote[$x]} ${sw_branch[$x]}
-                    git push ${sw_remote[$x]} ${sw_branch[$x]}
+                    git commit -m "${SW_commit[$x]}"
+                    git pull ${SW_remote[$x]} ${SW_branch[$x]}
+                    git push ${SW_remote[$x]} ${SW_branch[$x]}
                     echo "Done!"
                 elif [[ -z $2 ]]; then
-                    for i in ${!sw_alias[@]}; do
+                    for i in ${!SW_alias[@]}; do
                         sw -p ${alias[$i]}
                     done
                 else
@@ -156,10 +156,10 @@ install_dir=$HOME/.config/sw.sh
                 echo $error
             fi
 ## Unseting Auxiliary Functions
-            unset -f sw_option
-            unset -f sw_math
-            unset -f sw_cvt
-            unset -f inv_sw_alias
+            unset -f SW_option
+            unset -f SW_math
+            unset -f SW_cvt
+            unset -f inv_SW_alias
         }
    
 # ALIASES
